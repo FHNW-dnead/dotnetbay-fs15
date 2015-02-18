c04977ce9f0be8a16956d6323e419249dbe29e79
Michael Schnyder <michael.schnyder@zuehlke.com>
2015-02-18 22:18:06 +0100

    added tests for memberservice

---
+++ source/DotNetBay.Test/Core/MemberServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using DotNetBay.Core;
+using DotNetBay.Data.FileStorage;
+
+using NUnit.Framework;
+
+namespace DotNetBay.Test.Core
+{
+    public class MemberServiceTest
+    {
+        [TestCase]
+        public void WhenCurrentUserIsNotInRepo_GetCurrentUser_AlwaysGetAMember()
+        {
+            var repo = new InMemoryMainRepository();
+            var service = new SimpleMemberService(repo);
+
+            var currentMember = service.GetCurrentMember();
+
+            Assert.NotNull(currentMember);
+            Assert.IsNotNullOrEmpty(currentMember.DisplayName);
+            Assert.IsNotNullOrEmpty(currentMember.EMail);
+        }
+
+        [TestCase]
+        public void GettingCurrentMemberTwice_IsSame()
+        {
+            var repo = new InMemoryMainRepository();
+            var service = new SimpleMemberService(repo);
+
+            var currentMember1 = service.GetCurrentMember();
+            var currentMember2 = service.GetCurrentMember();
+            
+            Assert.AreEqual(currentMember1, currentMember2);
+        }
+    }
+}
