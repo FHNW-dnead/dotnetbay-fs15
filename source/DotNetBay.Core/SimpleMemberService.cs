c12f72fcd62d21a6000b0ea9c4a3b78dafa2e9ee
Michael Schnyder <michael.schnyder@zuehlke.com>
2015-02-18 22:16:34 +0100

    rename

--- source/DotNetBay.Core/SimpleMemberService.cs
+++ source/DotNetBay.Core/SimpleMemberService.cs
@@ -1,32 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 
 using DotNetBay.Interfaces;
 using DotNetBay.Model;
 
 namespace DotNetBay.Core
 {
-    public class MemberService : IMemberService
+    public class SimpleMemberService : IMemberService
     {
         private readonly IMainRepository repository;
 
-        public MemberService(IMainRepository repository)
+        public SimpleMemberService(IMainRepository repository)
         {
             this.repository = repository;
         }
 
         public Member GetCurrentMember()
         {
-            var principal = Thread.CurrentPrincipal;
-
-            var member = this.repository.GetMembers().FirstOrDefault(m => m.Name == principal.Identity.Name);
+            var uniqueId = string.Format("{0}@{1}.local", Environment.UserName, Environment.UserDomainName);
+            
+            var member = this.repository.GetMembers().FirstOrDefault(m => m.UniqueId == uniqueId);
 
             if (member == null)
             {
-                member = new Member() { Name = principal.Identity.Name, UniqueId = Guid.NewGuid().ToString() };
+                member = new Member
+                    {
+                        UniqueId = uniqueId, 
+                        DisplayName = Environment.UserName, 
+                        EMail = string.Format("{0}@{1}.local", Environment.UserName, Environment.UserDomainName)
+                    };
+
                 this.repository.Add(member);
+                this.repository.SaveChanges();
             }
 
             return member;
